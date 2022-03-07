99791f907a4afecad658900d9bd7f60015ff1690
hoan.phamphu <hoan.pham@genefriendway.com>
2022-03-07 22:08:28 +0700

    Lesson 01

---
+++ Lesson_01/hw_03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Datatype int: %lu bytes.\n", sizeof(int));
+    printf("Datatype char: %lu bytes.\n", sizeof(char));
+    printf("Datatype long: %lu bytes.\n", sizeof(long));
+    printf("Datatype double: %lu bytes.\n", sizeof(double));
+
+    return 0;
+}
